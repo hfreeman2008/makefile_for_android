81a04880b8e0db129265035b4b54e02d48a6abcb
hfreeman2008 <hfreeman2008@126.com>
2024-07-24 08:58:55 +0800

    close_selinx

---
+++ my_shell_script/close_selinx.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+date
+adb devices
+adb wait-for-device
+adb root
+adb wait-for-device
+adb remount
+adb wait-for-device
+adb devices
+
+adb shell getenforce
+adb shell setenforce 0
+adb shell getenforce
